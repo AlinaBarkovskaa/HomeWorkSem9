4047da43d0af0302de05b5dcb52908e3cc3ba604
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-12-17 23:50:16 +0400

    Added Task

---
+++ task64/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите чило: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Print (int n)
+{
+   int m = 1;
+   if(n >= m)
+   {
+       Console.WriteLine(n);
+       Print(n -1);
+   }
+ }
+Print(n);
