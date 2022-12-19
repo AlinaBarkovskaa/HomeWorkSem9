15f078c03c16b31993f4a35d23e074d9503f7a52
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-12-19 19:52:19 +0400

    Added task

---
+++ task68/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Acc(int M, int N) {
+    if (M == 0) {
+        return N + 1;
+    }
+    else if (N == 0 && M > 0) {
+        return Acc(M - 1, 1);
+    }
+    else {
+        return Acc(M - 1, Acc(M, N - 1));
+    }
+}
+Console.WriteLine(Acc(m, n));
--- task68/obj/Debug/net6.0/task68.AssemblyInfo.cs
+++ task68/obj/Debug/net6.0/task68.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("task64")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("task68")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("task64")]
-[assembly: System.Reflection.AssemblyTitleAttribute("task64")]
+[assembly: System.Reflection.AssemblyProductAttribute("task68")]
+[assembly: System.Reflection.AssemblyTitleAttribute("task68")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
